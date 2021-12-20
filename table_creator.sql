25ab342e3811569223675aa6eb5f4691e0c8abd2
Julie Lee <julielee9067@gmail.com>
2021-12-20 18:28:41 -0500

    Update table_creator to apply coursera

--- table_creator.sql
+++ table_creator.sql
@@ -1,23 +1,40 @@
 CREATE TABLE course (
-    course_id int NOT NULL AUTO_INCREMENT,
-    code_module varchar(45) NOT NULL,
-    code_presentation varchar(45) NOT NULL,
+    course_id varchar(100) NOT NULL,
+    institution varchar(45),
     PRIMARY KEY (course_id)
+);
+
+CREATE TABLE review (
+    review_id int AUTO_INCREMENT,
+    review varchar(1000),
+    course_id varchar(100),
+    reviewer varchar(45),
+    date date,
+    rating int,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (course_id) REFERENCES course(course_id)
 );
 
 CREATE TABLE student (
     student_id int NOT NULL,
-    course_id int NOT NULL,
-    num_of_prev_attempts int,
-    final_result varchar(45),
-    PRIMARY KEY (student_id),
-    FOREIGN KEY (course_id) REFERENCES course(course_id)
+    id_student int,
+    gender varchar(45),
+    region varchar(45),
+    highest_education varchar(45),
+    imd_band varchar(45),
+    age_band varchar(45),
+    studied_credits int,
+    disability varchar(45),
+    PRIMARY KEY (student_id)
 );
+
 
 CREATE TABLE student_registration (
     student_id int NOT NULL,
     course_id int NOT NULL,
     is_registered bool NOT NULL DEFAULT FALSE,
+    num_of_prev_attempts int,
+    final_result varchar(45),
     FOREIGN KEY (student_id) REFERENCES student(student_id),
     FOREIGN KEY (course_id) REFERENCES course(course_id)
 );

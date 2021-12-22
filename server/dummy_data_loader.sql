8b34f9ac9d9e941b2a0df100a691e2f1f1e8639e
Julie Lee <julielee9067@gmail.com>
2021-12-21 19:12:03 -0500

    Create dummy data loader

---
+++ server/dummy_data_loader.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO instructor (instructor_id, first_name, last_name)
+VALUES (1, 'John', 'Smith');
+
+INSERT IGNORE INTO review (review_id, student_id, review, course_id, date, rating)
+VALUES (1, 45462, 'Fantastic course!', 'AAA_2013J', '2021-12-21', 5);
